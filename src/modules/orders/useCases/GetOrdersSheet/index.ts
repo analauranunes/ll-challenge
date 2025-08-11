52e3640d6ce51ca52073cdf32e863539f5cda6e4
Ana Laura <analaurahdn@gmail.com>
2025-08-11 16:13:45 -0300

    fix: corrige importação do repositório de pedidos

--- src/modules/orders/useCases/GetOrdersSheet/index.ts
+++ src/modules/orders/useCases/GetOrdersSheet/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import OrderRepository from "../../repositories/ordersRepository";
+import OrderRepository from "../../repositories/OrdersRepository";
 import GetOrdersSheetController from "./GetOrdersSheetController";
 import GetOrdersSheetUseCase from "./GetOrdersSheetUseCase";
 
